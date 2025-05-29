cc77046b82ebd6358affdddb5b74a96f7af7b135
Lukas Kratochvil <lkratochvil57@gmail.com>
2025-05-29 16:21:37 +0200

    feat: added ESLint ignored dirs and files

--- packages/eslint-config/base.mjs
+++ packages/eslint-config/base.mjs
@@ -1,10 +1,13 @@
-import turbo from "eslint-plugin-turbo";
-import jsEslint from "@eslint/js";
-import tsEslint from "typescript-eslint";
-import prettierEslint from "eslint-config-prettier";
+import turbo from 'eslint-plugin-turbo';
+import jsEslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import prettierEslint from 'eslint-config-prettier';
 
 export default tsEslint.config(
-  turbo.configs["flat/recommended"],
+  {
+    ignores: ['public', 'node_modules', 'build', 'dist']
+  },
+  turbo.configs['flat/recommended'],
   prettierEslint,
   jsEslint.configs.recommended,
   ...tsEslint.configs.recommended,
