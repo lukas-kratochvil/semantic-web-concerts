df6e2e22fb8d7f36fd9968fb2bf85e4202ad48d0
Lukas Kratochvil <lkratochvil57@gmail.com>
2025-11-18 09:51:20 +0100

    feat: queue consumer - job data transformation and validation

--- apps/handler/src/queue/music-event.consumer.ts
+++ apps/handler/src/queue/music-event.consumer.ts
@@ -6,16 +6,49 @@ import {
   type MusicEventsQueueNameType,
 } from "@semantic-web-concerts/core";
 import type { Job, Worker } from "bullmq";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { MusicEventEntity } from "../entities/music-event.entity";
 
 @Processor(MusicEventsQueue.name)
 export class MusicEventConsumer extends WorkerHost<Worker<MusicEventsQueueDataType, MusicEventsQueueDataType>> {
   #logger = new Logger(MusicEventConsumer.name);
 
-  override async process(
-    job: Job<MusicEventsQueueDataType, MusicEventsQueueDataType, MusicEventsQueueNameType>
-  ): Promise<undefined> {
-    // TODO: process jobs - also, should job contain single MusicEvent or batch of MusicEvents??
-    this.#logger.log(job.data);
-    await job.updateProgress(-1);
+  /**
+   * Processing steps:
+   * 1) Transform to MusicEventEntity
+   * 2) Validate MusicEventEntity
+   * 3) Check if object already exists in the triple store
+   *    - If doesn't exist, continue with step 4)
+   *    - If exists, check if any property is updated
+   *        - If updated, continue with step 4)
+   *        - If not updated, return without further processing
+   * 4) Serialize MusicEventEntity to RDF
+   * 5) Store RDF in the triple store
+   *    - If step 3) determined that the object is new, perform an INSERT operation
+   *    - If step 3) determined that the object is updated, perform a DELETE + INSERT operation
+   */
+  override async process(job: Job<MusicEventsQueueDataType, MusicEventsQueueDataType, MusicEventsQueueNameType>) {
+    try {
+      // 1) Transform to MusicEventEntity
+      const musicEvent = plainToClass(MusicEventEntity, job.data.event);
+
+      // 2) Validate MusicEventEntity
+      const validationErrors = await validate(musicEvent);
+
+      if (validationErrors.length > 0) {
+        const validationErrorStr = validationErrors
+          .map((error) => `Property ${error.property}: ` + error.toString())
+          .join("\n");
+        throw new Error(MusicEventEntity.name + " validation failed:\n" + validationErrorStr);
+      }
+
+      // TODO: step 3) and further steps
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : JSON.stringify(error);
+      await job.log(errorMessage);
+      this.#logger.error(`Error processing job ${job.id}: ` + errorMessage);
+      throw error;
+    }
   }
 }
